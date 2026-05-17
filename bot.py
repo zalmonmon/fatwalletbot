26051900f59b3dae4d936671659e5a647529029d
zalmonmon <wong_zi_qing@hotmail.com>
2026-05-17 11:17:29 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -331,6 +331,8 @@ def home():
 # =========================
 # RUN BOTH
 # =========================
-if __name__ == "__main__":
-    threading.Thread(target=run_bot).start()
-    web_app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+import threading
+
+threading.Thread(target=run_bot, daemon=True).start()
+
+web_app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
